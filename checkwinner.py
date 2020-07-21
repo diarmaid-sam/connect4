d934c110ea937d65bed980b7b8d962eaff6b86df
Diarmaid Sam <diarmaidsam@outlook.com>
2020-07-21 22:02:43 +0100

    Started 'checkwinner' function module

---
+++ checkwinner.py
@@ -0,0 +1,14 @@
+
+#([[0, 0, 0, 0, 1, 0, 0],
+#    [0, 0, 0, 0, 1, 0, 0],
+#    [0, 0, 0, 0, 1, 0, 0],
+#    [0, 0, 1, 0, 1, 0, 0],
+#    [0, 1, 1, 1, 1, 1, 0],
+#    [1, 1, 1, 1, 1, 1, 1]])
+
+# these functions should check all possible combinations from the placed token. This would be more time efficient
+# like much much more time efficient. just pass
+def checkwinner(board, token, token_place):
+    pass
+
+
