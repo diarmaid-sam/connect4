0b372cc8fc45f374117dd9d98f49869874e1292d
Diarmaid Sam <diarmaidsam@outlook.com>
2020-07-21 18:50:39 +0100

    Finished 'select_place' function (inc error proofed)

--- GAME.py
+++ GAME.py
@@ -12,95 +12,63 @@ class Game:
         # their preferred token established when passed as parameter
         self.token1 = token1
         self.token2 = token2
+
     # the game board which will be directly interacted with by the
-    board = np.array([[0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 0, 0],
-                    [0, 1, 1, 1, 1, 1, 0],
-                    [1, 1, 1, 1, 1, 1, 1]])
+    board = np.array([[0, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 1, 0, 0],
+                      [0, 1, 1, 1, 1, 1, 0],
+                      [1, 1, 1, 1, 1, 1, 1]])
 
     def showboard(self):
         for i in Game.board:
             # prints the list in a more graphically appealing way
             print(" | ".join(str(n) for n in i))
 
-    def select_placee(self):
-        Game.showboard(self)
-        print(colored("\033[1m1   2   3   4   5   6   7\033[0m", "yellow"))
+    def select_place(self):
         invalid = True
+        # Invalid = True prevents the player from exiting the while loop untill they have placed a valid integer
         # while loop takes a valid placement for token. If invalid (i.e. placement in a full column) then it asks
         # to select another
+        token_place = "initialised"
         while invalid:
-            placement = (int(input("\nPlease input the number you would like to place your token : "))-1)
-            if Game.board[0][placement] != 0:
-                print(colored(f"\n------- The column you have selected (column {(placement + 1)}) is fully occupied "
-                              f"-------\n\n  -------- Please select another column to place your token --------\n",
-                              "red"))
-            else:
-                for row in Game.board:
-                    row_number = -1
-                    if Game.board[row_number, placement] == 0:
-                        token_place = Game.board[row_number, placement]
-                        print("non-zero")
-                        break
-                    else:
-                        row_number += -1
-                        print("reached")
-                return token_place
-
-
-
-
-
-
-
-    def select_place(self):
-        token_place = False
-        # token_place is the variable which records the exact index in which the player's token will be placed
-        # this variable is required to ensure that a suitable token place is found (if a column is full then
-        # the user will have to find another place for it
-
-        while not token_place:
             Game.showboard(self)
             print(colored("\033[1m1   2   3   4   5   6   7\033[0m", "yellow"))
-            Error = True
-            while Error:
-                try:
-                    # (for placement) this allows for the placement number to be directly used to describe the index of the
-                    # token placement
-                    placement = (int(input("\nPlease input the number you would like to place your token : "))-1)
-                    # token_place is the variable which records the exact index in which the player's token will be placed
-                    # '-1' value will start from the last value in the list
-                except ValueError:
+            try:
+                placement = (int(input("\nPlease input the number of the column you would like to place your "
+                                       "token : ")) - 1)
+                if Game.board[0][placement] != 0:
+                    print(
+                        colored(f"\n------- The column you have selected (column {(placement + 1)}) is fully occupied "
+                                f"-------\n\n  -------- Please select another column to place your token "
+                                f"--------\n",
+                                "red"))
+                    continue
+                    # this 'continue' immediately sends the player back to the start of the loop to re-input 'placement'
+            except ValueError:
+                print(colored(f"\n------- You have imputed no value |or| you have imputed a string value "
+                              f"-------\n\n  -------- Please input an integer value in range 1-7 --------\n",
+                              "red"))
+            except IndexError:
+                print(colored(f"\n------- You have imputed a value out of range 1-7 -------"
+                          f"-------\n\n  -------- Please input an integer value in range 1-7 --------\n",
+                          "red"))
 
+            else:
                 row_number = -1
-                for x in range (0,6):
+                print(placement)
+                for row in Game.board:
                     if Game.board[row_number, placement] == 0:
-                        token_place = Game.board[row_number, placement]
-                        print("non-zero")
+                        token_place = (row_number, placement)
+                        print("zero")
                         break
-                        #before you left, you are going to rearrange the file to check the first element of the selected
-                        #column of the game board. if it doesn't = 0 (i.e. isnt empty) you will ask them to choose another
+
                     else:
                         row_number += -1
                         print("reached")
-                if:
-                    print(colored(f"\n------- The column you have selected (column {(placement+1)}) is fully occupied "
-                                  f"-------\n\n  -------- Please select another column to place your token --------\n",
-                                  "red"))
-                else:
-                    ("Placement found")
+                return print(token_place)
 
 
-
-
-
-
-
-
-
-
-game1 = Game("diarmaid","bob","d","b")
-
+game1 = Game("diarmaid", "bob", "d", "b")
 game1.select_place()
