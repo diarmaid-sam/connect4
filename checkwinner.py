805f88218959726e3d668312501cdded4690ec4d
Diarmaid Sam <diarmaidsam@outlook.com>
2020-07-26 15:09:32 +0100

    cleaned up code a bit, reduced repeating myself by adding a 'universal' connect4 checking function

--- checkwinner.py
+++ checkwinner.py
@@ -14,37 +14,42 @@ def checkwinner(board, token, token_place):
     pass
 
 
-
 # ---------------------- Below are the functions for function 'checkwinner' ------------------------ #
-fake_list = np.array([[0, 0, 0, 0, 0, 1, 1],
-                      [1, 1, 0, 1, 0, 1, 1],
-                      [0, 1, 1, 1, 1, 0, 0],
-                      [1, 0, 1, 0, 0, 1, 1],
-                      [1, 1, 0, 1, 0, 0, 1],
-                      [0, 1, 1, 1, 1, 0, 0]])
+fake_list = np.array([[0, 0, 0, 1, 0, 0, 1],
+                      [1, 1, 0, 1, 1, 0, 1],
+                      [0, 1, 1, 1, 1, 1, 1],
+                      [0, 0, 1, 1, 1, 1, 1],
+                      [1, 0, 0, 1, 0, 1, 1],
+                      [0, 1, 1, 1, 1, 1, 1]])
+
+
+# this is the fundamental function which will check for the connect4 in each instance
+def check_connect4(array, token):
+    token_array = [n == token for n in array]
+    print(token_array)
+    token_count = (len(array) - 3)
+    print(token_count)
+    for token_index in range(0, token_count):
+        connect4_count = 0
+        for i in range(token_index, token_index + 4):
+            print(f"from {token_index} @ {i}")
+            if token_array[token_index]:
+                connect4_count += 1
+    print(token_index)
+    print(token_count)
+    if connect4_count == 4:
+        return True
+    elif token_index == (token_count - 1):
+        return False
+
 
 # Function to check for 4 same tokens in a horizontal row #
 def check_horizontal(board, token, token_place):
     checked_row = board[token_place[0]]
     print(checked_row)
-    # takes row of the placed token, distinguishes player's tokens from other tokens (marked with 'True')
-    player_board_placements = (checked_row == token)
-    print(player_board_placements)
-    # e.g. [True, False, True, True, True, True, False]
-    # only required to iterate over the first 4 elements to cover the whole row
-    for element in range(0, 4):
-        # creates the range in which the elements can be iterated over
-        connect4_counter = 0
-        for i in range(element, element+4):
-            if player_board_placements[i] == True:
-                connect4_counter += 1
+    return check_connect4(checked_row, token)
 
-        if connect4_counter == 4:
-            winner = True
-    if winner:
-        return winner
-    else:
-        return False
+
 # function(s) to convert vertical elements into a list and check for 4 same tokens in a row #
 
 # this function will convert all elements in the same position index in their respective lists (of a 2d array)
@@ -52,37 +57,21 @@ def check_horizontal(board, token, token_place):
 def convert_vertical_array(board, token_place):
     position_index = token_place[1]
     vertical_array = []
-    for i in range(0,6):
+    for i in range(0, 6):
         vertical_array.append(board[i][position_index])
     return vertical_array
+
 
 def check_vertical(board, token, token_place):
     vertical_array = convert_vertical_array(board, token_place)
     # once array is made, the process is very similar to 'check_horizontal' function. Check there for more info.
-    winner = False
-    for element in range(0,3):
-        connect4_counter = 0
-        for i in range(element, element+4):
-            if vertical_array[i] == token:
-                connect4_counter +=1
+    return check_connect4(vertical_array, token)
 
-        if connect4_counter == 4:
-            winner = True
-    if winner:
-        return winner
-    else:
-        return False
 
 # Below is function to check for 4 same tokens in a vertical fashion #
-# fake_list = np.array([[0, 0, 0, 0, 0, 1, 1],
-#                       [1, 1, 0, 1, 0, 1, 1],
-#                       [0, 1, 1, 1, 1, 0, 0],
-#                       [1, 0, @, 0, 0, 1, 1],
-#                       [1, 1, 0, 1, 0, 0, 1],
-#                       [0, 1, 1, 1, 1, d, 0]])
 
- # (0, 4), if reversed it would equal (0,2) #
- # 0 --> 6, 1 ---> 5, 2 ----> 4, 3 ----> 3 #
+# (0, 4), if reversed it would equal (0,2) #
+# 0 --> 6, 1 ---> 5, 2 ----> 4, 3 ----> 3 #
 
 # so gotta make 3 mini functions:
 
@@ -97,37 +86,60 @@ def check_vertical(board, token, token_place):
 #                     e.g.(list , (-3,2)
 
 
-
 def convert_diagonal_array(board, token_place):
     diagonal_array = []
     row = token_place[0]
     index = token_place[1]
-    board = board = board
     finding_start = True
     while finding_start:
         if row == -1 or index == 0:
             finding_start = False
         elif index == -1 and row == 0:
             finding_start = False
-        row += 1
-        index += -1
-    print(board[row][index])
+        else:
+            row += 1
+            index += -1
     diagonal_array.append(board[row][index])
     finding_end = True
+
     while finding_end:
-        print(row, index)
-        print(board[row][index])
         if row == -6 or index == 6:
-            if len(diagonal_array) >=4:
-                print(diagonal_array)
+            if len(diagonal_array) > 3:
                 return diagonal_array
+                # more efficient to remove diagonals with less than 4 elements (as connect4 is not possible)
             else:
                 return False
         else:
+            row += -1
+            index += 1
             diagonal_array.append(board[row][index])
-            row +=-1
-            index +=1
 
     # this is the position in the list where we will add elements to the 'diagonal array'
 
-convert_diagonal_array(fake_list, (-2, 6))
+
+# (0, 4), if reversed it would equal (0,2) #
+# 0 --> 6, 1 ---> 5, 2 ----> 4, 3 ----> 3 #
+
+def reverse_token_place(token_place):
+    token_index = [6, 5, 4, 3, 2, 1, 0]
+    new_token_place = (token_place[0], token_index.index(token_place[1]))
+    return new_token_place
+
+
+def check_diagonal(board, token, token_place):
+    diagonal_array = convert_diagonal_array(board, token_place)
+
+    if check_connect4(diagonal_array, token):
+        print("heheh")
+        return True
+    print("not here")
+
+    reversed_token_place = reverse_token_place(token_place)
+    diagonal_array = convert_diagonal_array(board, reversed_token_place)
+    if check_connect4(diagonal_array, token):
+        print("here")
+        return True
+    else:
+        return False
+
+print(check_diagonal(fake_list, 1, (-5, 4)))
